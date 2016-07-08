6332ab6e4bcb688a2ec7c91a36dc5aabca308e57
rabbasi <rabbasi@live.unc.edu>
2016-07-08 14:25:18 -0400

    added converted doecuments of Witchcraft in HTML,DOCX,ODT and PDF

--- rabbasi2-convert-docs.sh
+++ rabbasi2-convert-docs.sh
@@ -1,13 +1,15 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
+docs=$1
 
 # Convert markdown to HTML
-
+pandoc -o $docs.html $docs.md
 # Convert markdown to DOCX
-
+pandoc -o $docs.docx $docs.md
 # Convert markdown to ODT
-
+pandoc -o $docs.odt $docs.md
 # Convert markdown to PDF
-
+pandoc -o $docs.pdf $docs.md
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted" $docs "to HTML, DOCX, ODT,PDF"
